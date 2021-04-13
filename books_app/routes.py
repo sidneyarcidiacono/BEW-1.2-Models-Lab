908e5e26d1bfb7f7e46923ad48bf3553607f899f
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-04-13 09:39:59 -0700

    [update] Initial commit, completed all TODOs.

--- books_app/routes.py
+++ books_app/routes.py
@@ -1,8 +1,15 @@
 """Import packages and modules."""
 import os
-from flask import Blueprint, request, render_template, redirect, url_for, flash
+from flask import (
+    Blueprint,
+    request,
+    render_template,
+    redirect,
+    url_for,
+    flash,
+)
 from datetime import date, datetime
-from books_app.models import Book, Author, Genre
+from books_app.models import Book, Author, Genre, User
 
 # Import app and db from events_app package so that we can run app
 from books_app import app, db
@@ -13,13 +20,16 @@ main = Blueprint("main", __name__)
 #           Routes                       #
 ##########################################
 
-@main.route('/')
-def homepage():
-    # TODO: Make a query for all instances of 'User' and send to the template
-    return render_template('home.html')
 
-@main.route('/profile/<username>')
+@main.route("/")
+def homepage():
+    users = User.query.all()
+    return render_template("home.html", users=users)
+
+
+@main.route("/profile/<username>")
 def profile(username):
-    # TODO: Make a query for the user with the given username, and send to the
+    # Make a query for the user with the given username, and send to the
     # template
-    return render_template('profile.html', username=username)
+    user = User.query.filter_by(username=username).one()
+    return render_template("profile.html", username=user.username)
